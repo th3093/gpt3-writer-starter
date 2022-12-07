694c20239b76079ed84ee4f18801216be59d4894
th3093 <yannick.theobald@gmail.com>
2022-12-07 21:26:00 +0100

    deleted German suffix

--- pages/api/generate-ToC.js
+++ pages/api/generate-ToC.js
@@ -6,20 +6,20 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 const basePromptPrefix = `Write me an in depth table of contents for a research paper titled "`; //Please make sure the research paper goes in-depth on the topic and shows that the writer did their research.
-const basePromptSuffix = '" in German. Make sure to fit the scientific standard.\n Table of Contents:' // todo: dropdown list zur sprachauswahl implementieren!
+const basePromptSuffix = '". Make sure to fit the scientific standard.The table of content should contain at least 5 chapters.\n Table of Contents:' // todo: dropdown list zur sprachauswahl implementieren!
 
 const generateAction = async (req, res) => {
   // Run first prompt
 
 const baseCompletion = await openai.createCompletion({
-    model: 'text-curie-001',
+    model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}${basePromptSuffix}`,
     temperature: 0.96,
-    max_tokens: 250,
+    max_tokens: 500,
     frequency_penalty: 0.5,
     presence_penalty: -0.5, //noch versuchen für abwechslung und neue ideen
   });
-  
+  console.log(baseCompletion);
   const basePromptOutput = baseCompletion.data.choices.pop();
   res.status(200).json({ output: basePromptOutput});
 };
